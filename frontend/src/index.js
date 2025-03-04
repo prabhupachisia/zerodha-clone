5ac6e2429574873fe0338114808b89ecc883be11
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-03-04 13:45:52 +0530

    Initial Commit

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>My React App</h1>
+      <p>It's working!</p>
+    </div>
+  </React.StrictMode>
+);
