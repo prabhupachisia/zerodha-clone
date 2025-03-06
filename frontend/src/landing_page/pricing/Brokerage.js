87726c5868e47d0774747187d8508ed4137433b6
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-03-06 20:53:09 +0530

    Pricing Finished

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 function Brokerage() {
     return (
-        <h1>Brokerage</h1>
+        <div className='container border-top'>
+            <div className='row p-5 mt-5 text-center'>
+                <div className='col-8 p-4'>
+                    <a href="" style={{ textDecoration: "None" }}><h3 className='fs-5'>Brokerage calculator</h3></a>
+                    <ul style={{ textAlign: "left", lineHeight: "1.8" }} className='small'>
+                        <li className='text-muted'>Call & Trade and RMS auto-squareoff: Additional charges of ? 50 GST per order.</li>
+                        <li className='text-muted'>Digital contract notes will be sent via e-mail.</li>
+                        <li className='text-muted'>Physical copies of contract notes, if required, shall be charged ? 20 per contract note. Courier charges apply.</li>
+                        <li className='text-muted'>For NRI account (non-PIS), 0.5% or ? 100 per executed order for equity (whichever is lower).</li>
+                        <li className='text-muted'>For NRI account (PIS), 0.5% or 2200 per executed order for equity (whichever is lower).</li>
+                        <li className='text-muted'>If the account is in debit balance, any order placed will be charged 240 per executed order instead of 220 per executed order.</li>
+                    </ul>
+                </div>
+                <div className='col-4 p-4'>
+                    <a href="" style={{ textDecoration: "None" }}><h3 className='fs-5'>List of Charges</h3></a>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 function Hero() {
     return (
-        <h1>Hero</h1>
-     );
+        <div className='container p-5 '>
+            <div className='row text-center border-bottom'>
+                <h1>Charges</h1>
+                <h4 className='fs-5 mt-3 text-muted mb-5'>List of all charges and taxes</h4>
+            </div>
+            <div className='row mt-5 text-center'>
+                <div className='col p-4'>
+                    <img src="media/images/pricingEquity.svg" />
+                    <h1 className='fs-3'>Free equity delivery</h1>
+                    <p className="text-muted">All equity delivery investments (NSE, BSE),<br />
+                        are absolutely free — 0 brokerage.</p>
+                </div>
+                <div className='col p-4'>
+                    <img src="media/images/intradayTrades.svg" />
+                    <h1 className='fs-3'>Intraday and F&O trades</h1>
+                    <p className="text-muted">Flat 20 or 0.03% (whichever is lower) per<br />
+                        executed order on intraday trades across<br />
+                        equity, currency, and commodity t className="text-muted"rades. Flat<br />
+                        on all option trades.</p>
+                </div>
+                <div className='col p-4'>
+                    <img src="media/images/pricingEquity.svg" />
+                    <h1 className='fs-3'>Free direct MF</h1>
+                    <p className="text-muted">All direct mutual fund investments are<br />
+                        absolutely free — O commissions & DP<br />
+                        charges.</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
