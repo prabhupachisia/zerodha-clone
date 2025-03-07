8edae83058adc7beb3abe2e881fdc91795083bbb
prabhupachisia <prabhu.pachisia2023@vitstudent.ac.in>
2025-03-08 00:33:14 +0530

    Finished Support

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+    return (
+        <section className='container-fluid' id="supportHero">
+            <div className='p-5' id="supportWrapper">
+                <h4>Support Portal</h4>
+                <a href='#'>Track Tickets</a>
+            </div>
+            <div className=' row' id="supportContent">
+                <div className=' col-6 p-5 '>
+                    <h1 className='fs-3'>Search for an answer or browse help topics to create a ticket</h1>
+                    <input placeholder='Eg: how do i activate F&O, why is my order getting rejected' /><br />
+                    <a href=''>Track account opening</a>
+                    <a href=''>Track segment activation</a>
+                    <a href=''>Intraday margins</a>
+                    <a href=''>Kite user manual</a>
+                </div>
+                <div className=' col-6 p-5'>
+                    <h1>Featured</h1>
+                    <ol>
+                        <li><a href=''>Track account opening</a></li>
+                        <li><a href=''>Track segment activation</a></li>
+                    </ol>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
